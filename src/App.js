17a558c35072a9b505abb426c7add24bb57c8196
vitor <vitor.iwamura@sca.com.au>
2021-07-06 12:16:18 +1000

    Map of kenya

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Map from './components/Map';
+import Legend from './components/Legend';
 
 function App() {
   return (
       <div id="mapid">
-          <MapContainer center={[-33.87704450182364, 151.2066786256421]} zoom={5}>
-              <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              />
-          </MapContainer>
+          <div className='container'>
+              <Map/>
+              <Legend/>
+          </div>
       </div>
   );
 }
